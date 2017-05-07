8b535c1e14c2239402052ecdaffecffd4aeaddb6
SimonG96 <simon.gockner@gmail.com>
2017-05-07 22:36:28 +0200

    - add checks and logic for Überweisung

--- src/gui/Ueberweisung.java
+++ src/gui/Ueberweisung.java
@@ -5,15 +5,19 @@ package gui;
 * @author d.ferber
 */
 
+import bank.Bank;
+import bank.Konto;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
 
 public class Ueberweisung extends JFrame {
     JButton btUeberweisen = new JButton("Überweisen");
     JTextField tfVornameAbs = new JTextField(20);
     JTextField tfNachnameAbs = new JTextField(20);
     JTextField tfKontonummerAbs = new JTextField(20);
-    JTextField tfBetragAbs = new JTextField(20);
+    JTextField tfBetrag = new JTextField(20);
     JTextField tfVornameEmpf = new JTextField(20);
     JTextField tfNachnameEmpf = new JTextField(20);
     JTextField tfKontonummerEmpf = new JTextField(20);
@@ -21,7 +25,7 @@ public class Ueberweisung extends JFrame {
     JLabel lbVornameAbs = new JLabel("Vorname");
     JLabel lbNachnameAbs = new JLabel("Nachname");
     JLabel lbKontonummerAbs = new JLabel("Kontonummer");
-    JLabel lbBetragAbs = new JLabel("Betrag");
+    JLabel lbBetrag = new JLabel("Betrag");
     JLabel lbVornameEmpf = new JLabel("Vorname");
     JLabel lbNachnameEmpf = new JLabel("Nachname");
     JLabel lbKontonummerEmpf = new JLabel("Kontonummer");
@@ -44,7 +48,7 @@ public class Ueberweisung extends JFrame {
         add(tfVornameEmpf);
         add(tfNachnameEmpf);
         add(tfKontonummerEmpf);
-        add(tfBetragAbs);
+        add(tfBetrag);
         add(lbAbsender);
         add(lbVornameAbs);
         add(lbNachnameAbs);
@@ -53,7 +57,7 @@ public class Ueberweisung extends JFrame {
         add(lbNachnameEmpf);
         add(lbKontonummerEmpf);
         add(lbEmpfaenger);
-        add(lbBetragAbs);
+        add(lbBetrag);
         btUeberweisen.setBounds(75,300,150,20);
         tfVornameAbs.setBounds(10,40,150,20);
         tfNachnameAbs.setBounds(10,70,150,20);
@@ -61,7 +65,7 @@ public class Ueberweisung extends JFrame {
         tfVornameEmpf.setBounds(10,170,150,20);
         tfNachnameEmpf.setBounds(10,200,150,20);
         tfKontonummerEmpf.setBounds(10,230,150,20);
-        tfBetragAbs.setBounds(10,260,150,20);
+        tfBetrag.setBounds(10,260,150,20);
         lbAbsender.setBounds(75,10,150,20);
         lbVornameAbs.setBounds(170,40,150,20);
         lbNachnameAbs.setBounds(170,70,150,20);
@@ -70,6 +74,184 @@ public class Ueberweisung extends JFrame {
         lbVornameEmpf.setBounds(170,170,150,20);
         lbNachnameEmpf.setBounds(170,200,150,20);
         lbKontonummerEmpf.setBounds(170,230,150,20);
-        lbBetragAbs.setBounds(170,260,150,20);
+        lbBetrag.setBounds(170,260,150,20);
+
+        btUeberweisen.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                Ueberweisen();
+            }
+        });
+    }
+
+    void Ueberweisen(){
+        int kontonummerAbs = 0;
+        int kontonummerEmpf = 0;
+        double betrag = 0.0;
+        String vornameAbs = tfVornameAbs.getText();
+        String nachnameAbs = tfNachnameAbs.getText();
+        String vornameEmpf = tfVornameEmpf.getText();
+        String nachnameEmpf = tfNachnameEmpf.getText();
+
+        try{
+            kontonummerAbs = Integer.parseInt(tfKontonummerAbs.getText());
+        }
+        catch (NumberFormatException ex){
+            String message = "Geben Sie eine Zahl als Kontonummer für den Absender ein. \n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Falsches Format", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        try{
+            kontonummerEmpf = Integer.parseInt(tfKontonummerEmpf.getText());
+        }
+        catch (NumberFormatException ex){
+            String message = "Geben Sie eine Zahl als Kontonummer für den Empfänger ein. \n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Falsches Format", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        try{
+            betrag = Double.parseDouble(tfBetrag.getText());
+        }
+        catch (NumberFormatException ex){
+            String message = "Geben Sie eine Zahl als Betrag ein. \n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Falsches Format", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        if (vornameAbs.isEmpty() || nachnameAbs.isEmpty() || vornameEmpf.isEmpty() || nachnameEmpf.isEmpty())
+        {
+            String message = "Bitte füllen Sie folgende Felder aus: \n";
+
+            if (vornameAbs.isEmpty()){
+                message += "\t - Vorname Absender \n";
+            }
+
+            if (nachnameAbs.isEmpty()){
+                message += "\t - Nachname Absender \n";
+            }
+
+            if (vornameEmpf.isEmpty()){
+                message += "\t - Vorname Empfänger \n";
+            }
+
+            if (nachnameEmpf.isEmpty()){
+                message += "\t - Nachname Empfänger \n";
+            }
+
+            message += "\n Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Unvollständige Transaktion", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        Konto kontoAbs = Bank.Datenbank.GetKontoByKontonummer(kontonummerAbs);
+
+        if (!kontoAbs.getKunde().GetVorname().equals(vornameAbs)){
+            String message = "Der Vorname des Absenders stimmt nicht mit der angegebenen Kontonummer überein. \n" +
+                    "Ändern sie entweder den Vornamen oder die Kontonummer.\n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Ungültige Benutzerdaten", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        if (!kontoAbs.getKunde().GetNachname().equals(nachnameAbs)){
+            String message = "Der Nachname des Absenders stimmt nicht mit der angegebenen Kontonummer überein. \n" +
+                    "Ändern sie entweder den Nachnamen oder die Kontonummer.\n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Ungültige Benutzerdaten", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        Konto kontoEmpf = Bank.Datenbank.GetKontoByKontonummer(kontonummerEmpf);
+
+        if (!kontoEmpf.getKunde().GetVorname().equals(vornameEmpf)){
+            String message = "Der Vorname des Empfängers stimmt nicht mit der angegebenen Kontonummer überein. \n" +
+                    "Ändern sie entweder den Vornamen oder die Kontonummer.\n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Ungültige Benutzerdaten", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+        if (!kontoEmpf.getKunde().GetNachname().equals(nachnameEmpf)){
+            String message = "Der Nachname des Empfängers stimmt nicht mit der angegebenen Kontonummer überein. \n" +
+                    "Ändern sie entweder den Nachnamen oder die Kontonummer.\n\n" +
+                    "Drücken Sie Abbrechen um den Vorgang abzubrechen.";
+
+            int dialogResult = JOptionPane.showConfirmDialog(this, message, "Ungültige Benutzerdaten", JOptionPane.OK_CANCEL_OPTION, JOptionPane.ERROR_MESSAGE);
+
+            if (dialogResult == JOptionPane.CANCEL_OPTION){
+                this.dispose();
+                return;
+            }
+            else if (dialogResult == JOptionPane.OK_OPTION){
+                return;
+            }
+        }
+
+
+        kontoEmpf.Einzahlung(betrag);
+        kontoAbs.Auszahlung(betrag);
+
+        JOptionPane.showMessageDialog(this, "Überweisung von " + betrag + " war erfolgreich.", "Überweisung erfolgreich", JOptionPane.PLAIN_MESSAGE);
+        this.dispose();
     }
 }
