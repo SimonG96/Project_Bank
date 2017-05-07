725c11c3eb9a0f8afea621c4b9911879f78f87c9
SimonG96 <simon.gockner@gmail.com>
2017-05-08 00:51:47 +0200

    - remove TODOs

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -5,7 +5,7 @@
  */
 package bank;
 
-import gui.*;
+import gui.Oberflaeche;
 
 /**
  *
@@ -21,14 +21,6 @@ public class Bank {
     
     public static void main(String[] args) {
         Datenbank = new Datenbank();
-
-        //TODO: write new Kontostand after Transaction or at the end etc. ...
-        //TODO: check for Kunden before allowing any action except adding a new one
-        //TODO: create method "Auszahlung" in EinAuszahlung
-        //TODO: Add Betrag to Überweisung, add checks, call Einzahlug and Auszahlung for the two Konten
-        //TODO: create Kontoauszug
-
-
         new Oberflaeche();
     }
     
